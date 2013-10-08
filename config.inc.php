d9ff124a38d9149e53b129d7805f09c5728f3313
Uli Köhler <ulikoehler@online.de>
2013-10-09 00:30:57 +0200

    Fix multiline comment style

--- config.inc.php
+++ config.inc.php
@@ -16,7 +16,7 @@ $cfg["expire"]["1month"] = 30*24*60*60; // Well this is not *exactly* one month,
 $cfg["expire"]["1year"] = 365*24*60*60;
 $cfg["expire"]["never"] = -1;
 
-//
+//Labels for the expiration times. Must match those in $cfg["expire"]
 $cfg["expireLabels"]["5min"] = "5 minutes";
 $cfg["expireLabels"]["10min"] = "10 minutes";
 $cfg["expireLabels"]["1hour"] = "1 hour";
@@ -26,14 +26,19 @@ $cfg["expireLabels"]["1month"] = "1 month";
 $cfg["expireLabels"]["1year"] = "1 year";
 $cfg["expireLabels"]["never"] = "Never";
 
-// The expire value that is selected per default
-// Make sure the value exists in $cfg["expire"]
+/*
+ * The expire value that is selected per default
+ * Make sure the value exists in $cfg["expire"]
+ */
 $cfg["expireSelected"] = "1month";
 
-// Default expiration time, if nothing or sth invalid is selected
-// This is NOT the per-default selected option
-// Make sure the value exists in $cfg["expire"]
+/* Default expiration time, if nothing or sth invalid is selected
+ * This is NOT the per-default selected option
+ * Make sure the value exists in $cfg["expire"]
+ */
 $cfg["expireDefault"] = "never";
+
+
 
 
 ?>
