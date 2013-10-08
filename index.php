a69f2b22a9e705ab7fb6de2cbd73d97f8e8f20a8
Uli Köhler <ulikoehler@online.de>
2013-10-09 00:07:59 +0200

    Fix tpl variables

--- index.php
+++ index.php
@@ -403,6 +403,7 @@ else if (!empty($_SERVER['QUERY_STRING']))  // Return an existing paste.
 require_once "lib/rain.tpl.class.php";
 header('Content-Type: text/html; charset=utf-8');
 $page = new RainTPL;
+$page->assign('cfg',$cfg);
 $page->assign('CIPHERDATA',htmlspecialchars($CIPHERDATA,ENT_NOQUOTES));  // We escape it here because ENT_NOQUOTES can't be used in RainTPL templates.
 $page->assign('VERSION',$VERSION);
 $page->assign('ERRORMESSAGE',$ERRORMESSAGE);
